064b99f10fdd6cf595d4de8e23c7a9c970232c03
Surin Ninchai <Surin Ninchai>
2017-04-30 17:50:49 +0700

    saman commit 3

--- WebService/historyInsert.php
+++ WebService/historyInsert.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-echo json_encode(array("testing"=>TRUE));
+//echo json_encode(array("testing"=>TRUE));
 include("../Model/ConnectDatabase.php");
 
 if(isset($_GET["history"]) && isset($_GET["user_id"])&& isset($_GET["type"])){
